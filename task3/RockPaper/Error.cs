e5a100507405a1a43ab607b927270e4c35e08970
Saikat Mahmud <saikat8486@gmail.com>
2024-07-11 22:14:11 +0600

    task3 game

---
+++ task3/RockPaper/Error.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaper
+{
+    public static class Error
+    {
+        public const string NoMovesFound = "Enter your moves in sequence.";
+        public const string InvalidMovesLength = "Please enter odd number of moves greater than 3....";
+        public const string DuplicateMoves = "You entered duplicate moves! Try again....";
+        public const string InvalidUserInput = "! ! ! ! Enter your selection from the list....";
+    }
+}
---
+++ task3/RockPaper/Hash.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Org.BouncyCastle.Crypto.Digests;
+
+namespace RockPaper
+{
+    public class Hash
+    {
+        public string GetKey()
+        {
+            byte[] key = new byte[32];
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(key);
+            }
+            string keyString = BitConverter.ToString(key).Replace("-", "").ToUpperInvariant();
+            return keyString;
+        }
+
+        public string GetHMAC(string message, string key)
+        {
+            var myString = message + key;
+            byte[] data = Encoding.UTF8.GetBytes(myString);
+            Sha3Digest sha3 = new Sha3Digest(256);
+            sha3.BlockUpdate(data, 0, data.Length);
+            byte[] result = new byte[sha3.GetDigestSize()];
+            sha3.DoFinal(result, 0);
+            string hashString = BitConverter.ToString(result).Replace("-", "").ToUpperInvariant();
+            return hashString;
+        }
+    }
+}
