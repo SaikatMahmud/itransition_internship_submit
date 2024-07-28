9a98c566e722c4bdb3822fcaead5d5397468d1b3
Saikat Mahmud <saikat8486@gmail.com>
2024-07-28 22:50:45 +0600

    task5 upload

---
+++ task5/DataGenerator/Models/Region.cs
@@ -0,0 +1,10 @@
+﻿namespace DataGenerator.Models
+{
+    public static class Region
+    {
+        public const string USA = "en_US";
+        public const string Poland = "pl";
+        public const string France = "fr";
+        public const string Germany = "de";
+    }
+}
---
+++ task5/DataGenerator/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace DataGenerator.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        //public string? City { get; set; }
+       // public string? Country { get; set; }
+        //public string? Street { get; set; }
+        //public string? House { get; set; }
+        public string Phone { get; set; }
+    }
+}
